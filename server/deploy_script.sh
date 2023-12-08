70fe95ee3ebfb608190b322a19c75c1fdb62db6c
shahvatsal <vatsalshah2513@gmail.com>
2023-12-08 14:15:45 -0600

    update the code

--- server/deploy_script.sh
+++ server/deploy_script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# figure out how to copy the code into your ec2 instance
+
 # Ensure script fails on error
 set -e
 
 # Navigate to the directory where your application is located
-cd /Users/sim/Downloads/AppointDoc-main
+# cd /Users/sim/Downloads/AppointDoc-main  
+
+# when you push to circle ci the above path wont work because the path is for your
 
 # Pull the latest code from your repository
 # (This step assumes that your code is already cloned in the directory)
@@ -16,8 +20,8 @@ npm install
 
 # Build your application (if required)
 # For example, for a Node.js application:
-npm run build
+npm run start
 
 # Restart your application
 # This will depend on how your app is run. For instance, using PM2 for a Node.js app:
-pm2 restart appointdoctor
+# pm2 restart appointdoctor
